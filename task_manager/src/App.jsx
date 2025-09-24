843ab43babeb9a2d04d91e9ae8d65b93927e18a1
twin-25 <tjssg678@gmail.com>
2025-09-23 20:47:54 -0400

    Add Calender Page

--- task_manager/src/App.jsx
+++ task_manager/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import NewModal from './components/newModal/NewModal'
+import Mycalendar from './pages/calendar/Mycalendar'
 import Day from './pages/day/Day'
 import Home from './pages/home/Home'
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <>
       {/* <Home /> */}
-      <Day/>
+      {/* <Day/> */}
+      <Mycalendar/>
     </>
   )
 }
